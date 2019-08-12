72995d1a6e3b4aacb35296e91850849c08b44b09
Daniel Vianna <daniel.vianna@ausnetservices.com.au>
2019-08-12 11:03:39 +1000

    initial commit

---
+++ darwin-configuration.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+let
+
+  my-python-env = python-packages: with python-packages; [
+    jedi
+    rope
+    autopep8
+    yapf
+    black
+    flake8
+  ];
+
+  pythonEnv = with pkgs; python36Full.withPackages my-python-env;
+
+in
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs;
+    [
+      emacs
+      nix
+      cacert
+      pythonEnv
+      tree
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  # services.nix-daemon.enable = true;
+  nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  programs.bash.enable = true;
+  programs.zsh.enable = true;
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # You should generally set this to the total number of logical cores in your system.
+  # $ sysctl -n hw.ncpu
+  nix.maxJobs = 3;
+  nix.buildCores = 3;
+}
